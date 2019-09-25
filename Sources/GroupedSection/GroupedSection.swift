be5a69bc4d4354b612b82f042c29930ca3d183ff
Tim Fischer <tim.fischer@pentasys.de>
2019-09-25 17:02:59 +0200

    implementation

--- Sources/GroupedSection/GroupedSection.swift
+++ Sources/GroupedSection/GroupedSection.swift
@@ -1,3 +1,10 @@
-struct GroupedSection {
-    var text = "Hello, World!"
+struct GroupedSection<Identifier: Hashable, Row> {
+
+    var identifier : Identifier
+    var rows : [Row]
+
+    static func group(_ rows : [Row], by identifierForRow: (Row) -> Identifier) -> [GroupedSection<Identifier, Row>] {
+        return Dictionary(grouping: rows, by: identifierForRow).map(GroupedSection.init)
+    }
+
 }
